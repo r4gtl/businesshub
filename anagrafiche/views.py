32ef1726df9f16b015880715889ed41806a244c2
r4gtl <rquattrogtl@gmail.com>
2025-03-27 16:13:23 +0100

    Numero Fornitori

--- anagrafiche/views.py
+++ anagrafiche/views.py
@@ -3,12 +3,18 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Fornitore
 from .forms import FornitoreForm
+
+
 class FornitoreListView(LoginRequiredMixin, ListView):
     model = Fornitore
     template_name = "anagrafiche/fornitore_list.html"
     context_object_name = "fornitori"
 
-from .forms import FornitoreForm
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["totale_fornitori"] = Fornitore.objects.count()
+        return context
+
 
 class FornitoreCreateView(LoginRequiredMixin, CreateView):
     model = Fornitore
@@ -20,14 +26,15 @@ class FornitoreCreateView(LoginRequiredMixin, CreateView):
         form.instance.created_by = self.request.user
         return super().form_valid(form)
 
+
 class FornitoreUpdateView(LoginRequiredMixin, UpdateView):
     model = Fornitore
     form_class = FornitoreForm
     template_name = "anagrafiche/fornitore_form.html"
     success_url = reverse_lazy("anagrafiche:fornitore_list")
 
+
 class FornitoreDeleteView(LoginRequiredMixin, DeleteView):
     model = Fornitore
     template_name = "anagrafiche/fornitore_confirm_delete.html"
     success_url = reverse_lazy("anagrafiche:fornitore_list")
-
