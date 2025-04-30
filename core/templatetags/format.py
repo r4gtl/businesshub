22574d38f7c7209f3aa67681560946ad388ae302
r4gtl <rquattrogtl@gmail.com>
2025-04-30 15:34:57 +0200

    Template tag

--- core/templatetags/format.py
+++ core/templatetags/format.py
@@ -4,6 +4,8 @@ register = template.Library()
 @register.filter
 def format_importo(value):
     try:
+        if value == 0 or value == 0.0:
+            return ""
         return "{:,.2f}".format(value).replace(",", "X").replace(".", ",").replace("X", ".")
     except:
         return value
