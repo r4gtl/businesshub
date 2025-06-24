6d9bd73b6559ebd853e3ccdc6fc8ad85b4a1a24b
r4gtl <ferrari.stefano75@gmail.com>
2025-06-24 20:44:51 +0200

    Django Rest Framework

---
+++ accounts/api.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import RegisterSerializer, UserSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["username"] = user.username
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = RegisterSerializer
+
+
+class UserDetailView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+from .api import RegisterView, MyTokenObtainPairView, UserDetailView
 from . import views
 
 urlpatterns = [
+    # API endpoints
+    path("register/", RegisterView.as_view(), name="register"),
+    path("token/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("user/", UserDetailView.as_view(), name="user_detail"),
+    # Views classiche
     path("azienda/nuovo/", views.AziendaCreateView.as_view(), name="azienda_create"),
     path("user/nuovo/", views.UserCreateView.as_view(), name="user_create"),
 ]
